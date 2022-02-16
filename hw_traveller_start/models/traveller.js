e314964a12c0a7d461fde2afd4b429b9bb24cce4
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-16 16:36:57 +0000

    mvp and extensions complete

--- hw_traveller_start/models/traveller.js
+++ hw_traveller_start/models/traveller.js
@@ -3,29 +3,48 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
-
-};
-
-Traveller.prototype.getJourneyEndLocations = function () {
-  const output = this.journeys.filter(journey => journey.startLocation);
+  const output = [];
+  this.journeys.forEach((journey) => {
+    output.push(journey.startLocation);
+  }) 
   return output;
 };
 
+Traveller.prototype.getJourneyEndLocations = function () {
+  const output = [];
+  this.journeys.forEach((journey) => {
+    output.push(journey.endLocation);
+  }) 
+  return output;
+  
+};
+
 Traveller.prototype.getJourneysByTransport = function (transport) {
+  const output = this.journeys.filter(journey => journey.transport === transport)
+  return output;
 
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const output = this.journeys.filter(journey => journey.distance > minDistance);
+    return output;
+    
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const output = this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal += journey.distance;
+  }, 0);
+  return output;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const output = [];
+  this.journeys.forEach((journey) => {
+    output.push(journey.transport);
+  }) 
+  let unique_output = [...new Set(output)];
+  return unique_output;
 };
 
 
