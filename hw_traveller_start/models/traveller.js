9611f18a0b3e94de0ee4a5198f4ad85f65c1e743
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-16 16:54:02 +0000

    tidied code and optimised getUniqueModesOfTransport()

--- hw_traveller_start/models/traveller.js
+++ hw_traveller_start/models/traveller.js
@@ -16,19 +16,16 @@ Traveller.prototype.getJourneyEndLocations = function () {
     output.push(journey.endLocation);
   }) 
   return output;
-  
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
   const output = this.journeys.filter(journey => journey.transport === transport)
   return output;
-
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
   const output = this.journeys.filter(journey => journey.distance > minDistance);
     return output;
-    
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
@@ -39,13 +36,12 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  const output = [];
-  this.journeys.forEach((journey) => {
-    output.push(journey.transport);
-  }) 
-  let unique_output = [...new Set(output)];
-  return unique_output;
+const outputSet = new Set();
+this.journeys.forEach((journey) => {
+  outputSet.add(journey.transport);
+}) 
+  const output = Array.from(outputSet); 
+  return output;
 };
-
 
 module.exports = Traveller;
